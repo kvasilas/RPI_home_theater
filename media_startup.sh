250d43b605c58272a8afe186d8b9c151137ce112
Kirk Vasilas <35264676+kvasilas@users.noreply.github.com>
2019-06-05 13:48:30 -0400

    Update media_startup.sh

--- media_startup.sh
+++ media_startup.sh
@@ -1,3 +1,5 @@
 #cd to directory
 #pull repo or maybe not
-#python3 or pypy3 filename
+#python3 or pypy3 path/filename
+
+#set linnux ui to best settings for video ie hide all the tool bars and stuff then re open on close
